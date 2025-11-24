ca3be96e286412976f9b9466a9a53870037dafb1
dAspergillusb <dietolife@gmail.com>
2025-11-24 16:02:07 +0300

    trying to work with POST method and gtn data from form.

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
 from modules import register_main_endpoints
+from starlette.middleware.sessions import SessionMiddleware
 
 MAIN: FastAPI = FastAPI()
+
+MAIN.add_middleware(
+    SessionMiddleware,
+    secret_key="getsecretkey",
+    session_cookie="mycookie",
+    max_age=14 * 24 * 60 * 60
+)
 
 register_main_endpoints(app=MAIN)
 
--- modules/endpoints/main_pages.py
+++ modules/endpoints/main_pages.py
@@ -1,8 +1,22 @@
-from fastapi import FastAPI
+from fastapi import APIRouter, FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
+ROUTER: APIRouter = APIRouter(prefix="/pages", tags=["Frontend"])
+TEMPLATES: Jinja2Templates = Jinja2Templates(directory="modules/endpoints/templates")
 
 def register_main_endpoints(app: FastAPI) -> None:
+    app.mount("/static", StaticFiles(directory="modules/endpoints/static"), "static")
+
 
     @app.get(path="/")
-    def main_page():
-        return {"main": "page"}
+    def main_page(request: Request):
+        #print(dict(request))
+        request.get("")
+        return TEMPLATES.TemplateResponse(name="sign_in.html", context={"request": request})
+
+    @app.post(path="/")
+    def main_page_login(request: Request):
+        print(request.session.get("name"))
+        print(request.form())
+        #print(dict(request))
