2abb7fa386dce2b64591fe939445128bb10b0def
daspergillusb <dietolife@gmail.com>
2026-08-16 21:06:37 +0200

    Correct many bugs with up/down buttons, counting of common questions value, enable/disable action with button up/down.

--- modules/endpoints/static/js/check.inputs.js
+++ modules/endpoints/static/js/check.inputs.js
@@ -1,44 +1,58 @@
-function changeValue(inputId, buttonId) {
-  var buttonName = document.getElementById(buttonId).name;
-  var input = document.getElementById(inputId);
-  var value = parseInt(input.value);
-  if (buttonName == 'up') {
-    input.value = value + 1;
-  };
-  if (buttonName == 'down') {
-    if (input.value > 0) {
-      input.value = value - 1;
-    };
-  };
-  if (input.value > 0) {
-    document.getElementById('down_' + input.name.slice(-1)).disabled = false;
-  };
-  if (input.value == 0) {
-    document.getElementById('down_' + input.name.slice(-1)).disabled = true;
-  };
-  if (input.value == 10) {
-    document.getElementById('up_' + input.name.slice(-1)).disabled = true;
-  };
-  if (input.value < 10) {
-    document.getElementById('up_' + input.name.slice(-1)).disabled = false;
-  };
+function valueUp(input) {
+    input.setAttribute('value', parseInt(input.value) + 1)
+}
 
-  var commonSum = 0;
-  for (let i = 1; i < 28; i++) {
-    var inputValue = document.getElementById('q_' + i);
-    commonSum += parseInt(inputValue.value);
-  };
-  if (commonSum >= 30) {
-    for (let i = 1; i < 28; i++) {
-      var button = document.getElementById('up_' + i);
-      button.disabled = true;
+function valueDown(input) {
+    if (input.value > 0) {
+        input.setAttribute('value', parseInt(input.value) - 1)
+    }
+}
+
+function buttonDisabled(button) {
+    button.disabled = true;
+}
+
+function buttonEnabled(button) {
+    button.disabled = false;
+}
+
+function changeValue(inputId, buttonId) {
+    const buttonName = document.getElementById(buttonId).name;
+    const input = document.getElementById(inputId);
+    const upDown = {
+      'up': valueUp,
+      'down': valueDown
     };
-  } else {
-    for (let i = 1; i < 28; i++) {
-      if (document.getElementById('q_' + i).value < 10) {
-        var button = document.getElementById('up_' + i);
-        button.disabled = false;
-        };
-      };
-  };
-};
+    upDown[buttonName](input);
+
+    const buttonDown = document.getElementById(`down_${inputId.slice(2,)}`);
+    const buttonUp = document.getElementById(`up_${inputId.slice(2,)}`);
+    const buttonUpEvents = {
+        [parseInt(input.value) === 10]: buttonDisabled,
+        [parseInt(input.value) < 10]: buttonEnabled
+    }
+    const buttonDownEvents = {
+      [parseInt(input.value) > 0]: buttonEnabled,
+      [parseInt(input.value) === 0]: buttonDisabled
+    }
+    buttonUpEvents[true](buttonUp);
+    buttonDownEvents[true](buttonDown);
+
+    var commonSum = 0;
+    const inputs = document.querySelectorAll('input[name^="q_"][type="text"]');
+    inputs.forEach(input => {
+        commonSum += parseInt(input.value);
+    })
+    var buttons = document.querySelectorAll('button[name="up"]');
+    if (commonSum >= 30) {
+        buttons.forEach(button => {
+            button.disabled = true;
+        });
+    } else {
+        buttons.forEach(button => {
+            if (document.getElementById(`q_${button.id.slice(3,)}`).value < 10) {
+                button.disabled = false;
+            }
+        });
+      }
+}
