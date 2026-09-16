744a5bcd8118a8f1c612c666ac4d6a833754ccd4
daspergillusb <dietolife@gmail.com>
2026-09-16 21:20:29 +0200

    a little change

--- modules/functions/test_time.py
+++ modules/functions/test_time.py
@@ -2,4 +2,4 @@ from ..endpoints.config import PROBLEM_TYPE_TO_TIME
 
 
 def get_time_of_test(problems_types: list[int]) -> int:
-    return sum([PROBLEM_TYPE_TO_TIME.get(problem_type) for problem_type in problems_types])
+    return sum([PROBLEM_TYPE_TO_TIME.get(problem_type, 0) for problem_type in problems_types])
