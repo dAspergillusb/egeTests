cc7153a195c4575f7ef39ceefae8fb75e79244a7
dAspergillusb <dietolife@gmail.com>
2026-09-17 11:03:56 +0200

    infra: add informatics Alembic environment

---
+++ alembic/informatics/env.py
@@ -0,0 +1,9 @@
+from alembic.common import run_migrations
+from modules.endpoints.config import DB_URL_PART, env_settings
+from modules.databases.MainDB import BASE_INF
+import modules.databases.InformaticsDB  # noqa: F401
+
+run_migrations(
+    f"{DB_URL_PART}{env_settings.MAIN_DB_INFORMATICS_NAME}",
+    BASE_INF.metadata,
+)
